d0889fa841ab3c9c538180e497ac4dc55224b633
Guillermo Gutierrez <guillegr123@gmail.com>
2020-05-15 09:17:00 -0600

    Setup video channel and handler

--- lib/phoenix_webrtc_web/channels/user_socket.ex
+++ lib/phoenix_webrtc_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PhoenixWebrtcWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PhoenixWebrtcWeb.RoomChannel
+  channel "video:*", PhoenixWebrtcWeb.VideoChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
