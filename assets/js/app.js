d7f8822d549403945db0124fe6e79c091961051f
Guillermo Gutierrez <guillegr123@gmail.com>
2020-05-16 11:47:51 -0600

    Wire up buttons and set initial app state

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,31 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+import channel from './socket';
+
+const connectButton = document.getElementById('connect');
+const callButton = document.getElementById('call');
+const disconnectButton = document.getElementById('disconnect');
+
+const remoteVideo = document.getElementById('remote-stream');
+const localVideo = document.getElementById('remote-stream');
+const remoteStream = new MediaStream();
+
+setVideoStream(remoteVideo, remoteStream);
+
+let peerConnection;
+
+disconnect.disabled = true;
+call.disabled = true;
+connectButton.onclick = connect;
+callButton.onclick = call;
+disconnectButton.onclick = disconnect;
+
+const reportError = where => error => {
+    console.error(where, error)
+}
+
+function log() {
+    console.log(...arguments)
+}
