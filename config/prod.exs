9441475549d25c72b742f55682d03e704f4a6f95
Guillermo Gutierrez <guillegr123@gmail.com>
2020-05-21 13:16:37 -0600

    Add HTTP port to PROD config

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :phoenix_webrtc, PhoenixWebrtcWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "#{System.get_env("HOSTNAME")}", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
