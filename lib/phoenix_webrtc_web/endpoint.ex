90b6ae6f46c2713fdf48a20801849432b7d182ee
Guillermo Gutierrez <guillegr123@gmail.com>
2020-05-21 13:19:30 -0600

    Set websocket timeout to be less than Heroku's
    Decrease the timeout for the websocket transport to ensure that any idle
    connections are closed by Phoenix before they reach Heroku's 55-second
    timeout window.

--- lib/phoenix_webrtc_web/endpoint.ex
+++ lib/phoenix_webrtc_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule PhoenixWebrtcWeb.Endpoint do
   ]
 
   socket "/socket", PhoenixWebrtcWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
